e56e518e4fb7ee8d3616f46c9630d189f98daf45
Y@n <Zinchenko.yan@gmail.com>
2021-11-30 20:26:05 +0200

    Add Redux-Toolkit

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useState } from "react";
 import { Theme, ThemeContext } from "../context/ThemeContext";
 import { changeCssRootVariables } from "../model/ChangeCssRootVariables";
+import { storage } from "../model/Storage";
 
 interface Props {
     children: ReactNode;
@@ -8,9 +9,11 @@ interface Props {
 
 
 export const ThemeProvider = ({children, ...props }: Props) => {
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+    const [theme, setTheme] = useState<Theme>(storage.getItem('theme') || Theme.LIGHT);
 
+    changeCssRootVariables(theme);
     function changeTheme(theme: Theme) {
+        storage.setItem('theme', theme);
         setTheme(theme);
         changeCssRootVariables(theme);
     }
